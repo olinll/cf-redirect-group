6cc6399bcebd00963e4a56ca1476d2ec553df530
二叉树树 <acofork@qq.com>
2026-01-17 18:57:30 +0800

    Add short link: MEsrCq2

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2666,5 +2666,9 @@ window.RULES_INTERMEDIATE = {
     "/pO3y7no": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/MEsrCq2": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
