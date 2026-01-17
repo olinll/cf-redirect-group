5f3bf32038b1bd466e8d4a09cc8f7d5d0411c4ad
二叉树树 <acofork@qq.com>
2026-01-17 18:49:54 +0800

    Add short link: e213Dyg

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2326,5 +2326,9 @@ window.RULES_INTERMEDIATE = {
     "/Fv6lWzo": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/e213Dyg": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
