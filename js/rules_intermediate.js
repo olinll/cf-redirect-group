fc03cd93cce230126de6a73a0b8fa1d6fe4fa229
二叉树树 <acofork@qq.com>
2026-01-17 18:48:39 +0800

    Add short link: EXia064

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2222,5 +2222,9 @@ window.RULES_INTERMEDIATE = {
     "/LEKIHlj": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/EXia064": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
