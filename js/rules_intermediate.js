f3617ddbaee9fcd45712775495c6f6d09d0831cd
二叉树树 <acofork@qq.com>
2026-01-17 18:48:37 +0800

    Add short link: LEKIHlj

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2218,5 +2218,9 @@ window.RULES_INTERMEDIATE = {
     "/nVBLE0N": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/LEKIHlj": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
