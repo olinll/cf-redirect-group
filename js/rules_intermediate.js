27e846115666bc4f78c1ca15ce34e0dd379e0b60
二叉树树 <acofork@qq.com>
2026-01-17 18:56:12 +0800

    Add short link: 3HrNvsA

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2546,5 +2546,9 @@ window.RULES_INTERMEDIATE = {
     "/ebmdi9t": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/3HrNvsA": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
