b0ffc2daf88350be824348b790c0397047b48c14
二叉树树 <acofork@qq.com>
2026-01-17 19:00:59 +0800

    Add short link: F52kNup

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2738,5 +2738,9 @@ window.RULES_INTERMEDIATE = {
     "/97gOh8J": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/F52kNup": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
