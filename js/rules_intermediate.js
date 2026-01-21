7092dcb014087195c480f92d24b20c9fc6e2bed7
二叉树树 <acofork@qq.com>
2026-01-21 20:05:13 +0800

    Add short link: hlqi9

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -6454,5 +6454,9 @@ window.RULES_INTERMEDIATE = {
     "/d7399fxq": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T12:05:11.000Z"
+    },
+    "/hlqi9": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T12:05:13.000Z"
     }
 };
