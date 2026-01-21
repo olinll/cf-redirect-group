4f549978a54cd3ec1998c27c510435b5b72084ee
二叉树树 <acofork@qq.com>
2026-01-21 20:15:48 +0800

    Add short link: szaofz8c6k

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7478,5 +7478,9 @@ window.RULES_INTERMEDIATE = {
     "/iqkfdppuh": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T12:15:46.000Z"
+    },
+    "/szaofz8c6k": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T12:15:48.000Z"
     }
 };
