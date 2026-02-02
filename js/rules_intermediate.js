1664e8d3874b0c805b1e9dd0ab17e6d20b4a8b7c
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-02-02 16:43:51 +0800

    Add short link: 123451

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2,5 +2,9 @@ window.RULES_INTERMEDIATE = {
     "/12345": {
         "url": "https://www.baidu.com",
         "expired_at": "2026-02-03T08:21:00.000Z"
+    },
+    "/123451": {
+        "url": "https://github.com/olinll/cf-redirect-group/blob/main/.gitignore",
+        "expired_at": "2026-02-05T08:43:00.000Z"
     }
 };
