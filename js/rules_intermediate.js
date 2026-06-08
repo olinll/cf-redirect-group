e01d987c938ee69c738796485019d640c4c574d8
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-06-08 13:41:49 +0800

    Add short link: bukao

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,3 +1,6 @@
 window.RULES_INTERMEDIATE = {
-   
+    "/bukao": {
+        "url": "https://peisong.meituan.com/app/riderRecruitmentFusion/index",
+        "expired_at": "2026-06-10T05:41:00.000Z"
+    }
 };
