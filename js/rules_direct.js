46292ddca7697f7a0e72b81ed316b40740c271bb
顾拾柒 <olinl@foxmail.com>
2026-06-20 12:24:53 +0800

    url: 删除友链url，采用博客重定向

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -1,8 +1,4 @@
 window.RULES_DIRECT = {
-    "/f": {
-        "url": "https://blog.olinl.com/friends/",
-        "expired_at": ""
-    },
     "/s": {
         "url": "https://2r.hk/_url",
         "expired_at": ""
