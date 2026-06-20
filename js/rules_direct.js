3a5e0ce9e0d4f11f8d6149d36e376521de05b178
顾拾柒 <olinl@foxmail.com>
2026-06-20 11:58:01 +0800

    chore: 删除无用链接

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -2,17 +2,5 @@ window.RULES_DIRECT = {
     "/s": {
         "url": "https://2r.hk/_url",
         "expired_at": ""
-    },
-    "/d": {
-        "url": "https://note.olinl.com",
-        "expired_at": ""
-    },
-    "/i": {
-        "url": "https://i2.olinl.com",
-        "expired_at": ""
-    },
-    "/w": {
-        "url": "https://pan.olinl.com/d/local/HEU_KMS_Activator_v63.3.4.iso?sign=mHtsHCiBQiuvmJRAaiGG_0goHo7TyuwTOLawLDTHYis=:0",
-        "expired_at": ""
-    },
+    }
 };
--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,6 +1,2 @@
 window.RULES_INTERMEDIATE = {
-    "/bukao": {
-        "url": "https://peisong.meituan.com/app/riderRecruitmentFusion/index",
-        "expired_at": "2026-06-10T05:41:00.000Z"
-    }
 };
