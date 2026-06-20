570f9f7626cb59eaaab0743db139a45afaca74ce
顾拾柒 <olinl@foxmail.com>
2026-06-20 12:22:30 +0800

    url: 添加/bf 友链

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -1,4 +1,8 @@
 window.RULES_DIRECT = {
+    "/bf": {
+        "url": "https://blog.olinl.com/friends/",
+        "expired_at": ""
+    },
     "/s": {
         "url": "https://2r.hk/_url",
         "expired_at": ""
