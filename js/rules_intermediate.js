cae0f44d541ec72ca7116e371f0e085be9f5429f
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-06-21 15:24:17 +0800

    Add short link: sadasd

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,2 +1,6 @@
 window.RULES_INTERMEDIATE = {
+    "/sadasd": {
+        "url": "https://blog.olinl.com/posts/firefly-first/#cloudflare-worker-%E5%9F%9F%E5%90%8D%E4%BC%98%E9%80%89",
+        "expired_at": "2026-06-26T07:22:00.000Z"
+    }
 };
