ed4f53d938dae710129476b930ca9140931680fb
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-06-25 10:10:35 +0800

    Update rules_direct.js

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -2,5 +2,9 @@ window.RULES_DIRECT = {
     "/s": {
         "url": "https://2r.hk/_url",
         "expired_at": ""
+    },
+    "/music": {
+        "url": "https://tools.olinl.com/music",
+        "expired_at": ""
     }
 };
