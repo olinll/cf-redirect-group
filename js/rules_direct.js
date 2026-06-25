b6d8949523ad277eaf5155f72df70ff1e699a61f
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-06-25 10:15:48 +0800

    Update rules_direct.js

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -3,6 +3,7 @@ window.RULES_DIRECT = {
         "url": "https://2r.hk/_url",
         "expired_at": ""
     },
+    
     "/music": {
         "url": "https://tools.olinl.com/music",
         "expired_at": ""
