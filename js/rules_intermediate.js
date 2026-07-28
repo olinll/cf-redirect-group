d2ce90d85c85c2a94feeaeba87fcffd711539993
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-07-28 21:46:32 +0800

    Add short link: aaaaa

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -6,5 +6,9 @@ window.RULES_INTERMEDIATE = {
     "/lin-cffast": {
         "url": "https://blog.olinl.com/posts/firefly-first/#cloudflare-worker-%E5%9F%9F%E5%90%8D%E4%BC%98%E9%80%89",
         "expired_at": "2026-06-30T07:24:00.000Z"
+    },
+    "/aaaaa": {
+        "url": "https://sticker.oooo.so/",
+        "expired_at": "2026-07-31T13:46:00.000Z"
     }
 };
