72dd095076e6137595deff5fd21b34e3ddda37df
顾拾柒 <96218030+olinll@users.noreply.github.com>
2026-07-28 21:50:05 +0800

    Update rules_intermediate.js

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7,8 +7,16 @@ window.RULES_INTERMEDIATE = {
         "url": "https://blog.olinl.com/posts/firefly-first/#cloudflare-worker-%E5%9F%9F%E5%90%8D%E4%BC%98%E9%80%89",
         "expired_at": "2026-06-30T07:24:00.000Z"
     },
-    "/aaaaa": {
+    "/1": {
         "url": "https://sticker.oooo.so/",
+        "expired_at": "2026-07-31T13:46:00.000Z"
+    },
+    "/2": {
+        "url": "https://mirulife.top/",
+        "expired_at": "2026-07-31T13:46:00.000Z"
+    },
+    "/3": {
+        "url": "https://kiseki.blog/",
         "expired_at": "2026-07-31T13:46:00.000Z"
     }
 };
